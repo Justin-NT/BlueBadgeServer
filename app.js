314f4c8f95acac9017620b596d26f2ac97fe5fad
Justin <berranmicheals@gmail.com>
2020-02-06 10:07:40 -0500

    uploading force true to reset server because models were changed

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const sequelize = require("./db");
 const user = require("./controllers/usercontroller");
 const gameLog = require("./controllers/gamelogcontroller");
 
-sequelize.sync();
-// sequelize.sync({ force: true });
+// sequelize.sync();
+sequelize.sync({ force: true });
 app.use(express.json());
 
 app.use(require("./middleware/headers"));
